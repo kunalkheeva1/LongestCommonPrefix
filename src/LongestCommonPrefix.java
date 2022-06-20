82f67f183d7ffbd2b7d01b42e33ea4ed31c8ec08
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-19 19:42:11 -0700

    created a method for LongestCommonPrefix in the array of strings

--- src/LongestCommonPrefix.java
+++ src/LongestCommonPrefix.java
@@ -1,9 +1,28 @@
-public class LongestCommonPrefix {
-    public static String longestCommonPrefix(String [] str){
-        
+import java.util.*;
 
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String [] str){
+        StringBuilder result = new StringBuilder();
+        Arrays.sort(str);
+
+        char [] first = str[0].toCharArray();
+        char [] last = str[str.length - 1].toCharArray();
+
+        for(int i= 0; i<first.length; i++){
+            if(first[i]!=last[i])
+                break;
+
+            result.append(first[i]);
+        }  return result.toString();
     }
+    //above method did not work so I am trying to use another method anyhow both differ with time and space complexity
+//    public String longestCommonPrefix1(String[]str){
+//
+//    }
     public static void main(String[] args) {
+        String [] str = {"Kunal","Kuna","kunnu"};
+
+
 
     }
 }
